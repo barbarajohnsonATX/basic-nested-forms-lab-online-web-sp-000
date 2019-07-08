b7616cd2f6c188990609571ef90d0297a20278f7
Barbara Johnson <johnson.barbara@gmail.com>
2019-07-08 01:59:10 +0000

    Automatically backed up by Learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,8 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
 
-
+  def artist_attributes=(artist)
+    self.artist = Artist.find_or_create_by(name: artist[:name])
+    self.artist.update(artist)
+  end
 end

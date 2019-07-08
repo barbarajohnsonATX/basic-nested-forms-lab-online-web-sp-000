ba1ac5768163d8e4312ede63f6893f5e298355fd
Barbara Johnson <johnson.barbara@gmail.com>
2019-07-08 02:07:40 +0000

    Automatically backed up by Learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,10 +1,4 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
-
-  #recipe accepts two indgredients
-  def ingredients_attributes=(ingredients_attributes)
-    ingredients_attributes.each do |i, ingredient_attributes|
-      self.ingredients.build(ingredient_attributes)
-    end
-  end
+  accepts_news
 end
